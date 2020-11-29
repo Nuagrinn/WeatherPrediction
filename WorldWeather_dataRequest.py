f5393fc0dfde7ce6df6c37363c0cd4e37ba8b615
Nuagrinn <megoboom17@gmail.com>
2020-11-29 21:12:38 +0300

    add python script to request data from http://www.worldweatheronline.com/

---
+++ WorldWeather_dataRequest.py
@@ -0,0 +1,19 @@
+from wwo_hist import retrieve_hist_data
+import pandas as pd
+import requests
+
+start_date = '01-OCT-2017'
+end_date = '01-OCT-2020'
+api_key = '' # Here you put in you api-token
+location_list = ['moscow']
+
+hist_weather_data = retrieve_hist_data(api_key,
+                                location_list,
+                                start_date,
+                                end_date,
+                                frequency = 8,
+                                location_label = False,
+                                export_csv = True,
+                                store_df = True)
+
+
